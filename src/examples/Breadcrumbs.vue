dfd8d150774dfd16a6a693fc4bab48694d33de79
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-10-08 19:11:17 +0900

    v-if, v-else + class + :class 뷰의 조건부 렌더링 혹은 조건부 클래스

--- src/examples/Breadcrumbs.vue
+++ src/examples/Breadcrumbs.vue
@@ -1,42 +1,64 @@
 <template>
   <nav aria-label="breadcrumb">
+    <!-- 기본 class + :class
+    :class는 store === vuex에 접근해 삼항 연산자 -->
     <ol
       class="px-0 pt-1 pb-0 mb-0 bg-transparent breadcrumb"
       :class="$store.state.isRTL ? '' : ' me-sm-6'"
     >
       <li class="text-sm breadcrumb-item" :class="textWhite">
+        <!-- v-if, v-else
+        == if(condition) {
+
+        } else {
+
+        } == condition ? if : else -->
         <a
           v-if="$store.state.isRTL"
           :class="textWhite"
           class="opacity-5 ps-2"
           href="#"
-        >لوحات القيادة</a>
+          >لوحات القيادة</a
+        >
         <a v-else :class="textWhite" class="opacity-8" href="#">Pages</a>
       </li>
       <li
         class="text-sm breadcrumb-item active"
         :class="textWhite ? 'text-white' : 'text-dark'"
         aria-current="page"
-      >{{ currentPage }}</li>
+      >
+        <!-- currentPage prop, required 확인 -->
+        {{ currentPage }}
+      </li>
     </ol>
-    <h6 class="mb-0 font-weight-bolder" :class="textWhite ? 'text-white' : ''">{{ currentPage }}</h6>
+    <h6 class="mb-0 font-weight-bolder" :class="textWhite ? 'text-white' : ''">
+      <!-- currentPage prop, required 확인 -->
+      {{ currentPage }}
+    </h6>
   </nav>
 </template>
 
+<!-- 컴포넌트 선언 스크립트 단 -->
 <script>
 export default {
+  /* 고유 name */
   name: "BreadcrumbsComponent",
+  /* props */
   props: {
+    /* [key: String]: {
+      required: boolean,
+      type: String | Number | Boolean | Function,
+      default: String | Number | Boolean | Function,
+    } */
     currentPage: {
       required: true,
       type: String,
-      default: ""
+      default: "",
     },
     textWhite: {
       type: String,
-      default: ""
+      default: "",
     },
   },
-
 };
 </script>
