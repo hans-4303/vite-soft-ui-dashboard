14a622b48f3c847b8ed689e0034556982010f9bf
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-10-09 10:44:40 +0900

    v-bind와 $attrs 개념 찾아보기

--- src/examples/PageLayout/Navbar.vue
+++ src/examples/PageLayout/Navbar.vue
@@ -1,10 +1,14 @@
 <template>
   <!-- Navbar -->
+  <!-- props 따라 :class 대입하고 삼항 연산 -->
   <nav
     class="top-0 navbar navbar-expand-lg position-absolute z-index-3"
     :class="isBlur ? isBlur : 'shadow-none my-2 navbar-transparent w-100'"
   >
     <div class="container">
+      <!-- router-link 컴포넌트
+      to: /dashboard로 경로 설정
+      v-bind?? 그리고 $attrs? 찾아보기 -->
       <router-link
         class="navbar-brand font-weight-bolder ms-lg-0 ms-3"
         to="/dashboard"
@@ -88,13 +92,22 @@
   <!-- End Navbar -->
 </template>
 
+<!-- 컴포넌트 호출 및 선언하는 스크립트 단 -->
 <script>
+/* 이미지 호출 */
 import downArrWhite from "@/assets/img/down-arrow-white.svg";
 import downArrBlack from "@/assets/img/down-arrow-dark.svg";
 
+/* 상위 컴포넌트 선언 */
 export default {
+  /* 고유 name */
   name: "NavbarComponent",
+  /* props 선언 */
   props: {
+    /* [key: String]: {
+      type: Function;
+      default: String;
+    } */
     btnBackground: {
       type: String,
       default: ""
@@ -103,18 +116,27 @@ export default {
       type: String,
       default: ""
     },
+    /* [key: String]: {
+      type: Function;
+      default: Boolean;
+    } */
     darkMode: {
       type: Boolean,
       default: false,
     },
   },
+  /* data, 함수 형이며 객체 반환, 키는 컴포넌트에서 사용 가능 */
   data() {
     return {
       downArrWhite,
       downArrBlack,
     };
   },
+  /* computed, 의존성 따라 로직 실행 */
   computed: {
+    /* darkModes() 메서드 {
+      [key: String]: this.darkMode == [key: String]: { type: Function; default: Boolean; }
+    } */
     darkModes() {
       return {
         "text-dark": this.darkMode,
